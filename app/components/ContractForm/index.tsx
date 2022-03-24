32c6bd918d6387ca26ea28f64fabb31b4d107a52
PJColombo <paulo.colombo@pm.me>
2022-03-24 15:12:50 +0100

    feat(Home): add initial contract form

--- app/components/ContractForm/index.tsx
+++ app/components/ContractForm/index.tsx
@@ -1,115 +1,111 @@
-// import { Box, Button, DropDown, Field, Info, TextInput } from "@1hive/1hive-ui";
-// import { utils } from "ether";
-// import {
-//   ChangeEvent,
-//   FormEventHandler,
-//   useEffect,
-//   useMemo,
-//   useState,
-// } from "react";
-// import { NETWORK_IDS, NETWORKS, isTestnet } from "../../../utils";
-// import { NetworkItem } from "./NetworkItem";
-// const DEFAULT_NETWORK_ID_INDEX = -1;
+import {
+  Box,
+  Button,
+  DropDown,
+  Field,
+  GU,
+  Info,
+  TextInput,
+} from "@1hive/1hive-ui";
+import { utils } from "ethers";
+import { ChangeEvent, FormEventHandler, useEffect, useState } from "react";
+import styled from "styled-components";
+import { Chain } from "wagmi";
+import { getNetworkLogo } from "~/utils";
+import { NetworkItem } from "./NetworkItem";
 
-export const ContractForm = () => <div style={{}}>ContractForm</div>;
+const DEFAULT_NETWORK_ID_INDEX = -1;
 
-// type ContractFormProps = {
-//   onSubmit(contractAddress: string, networkId: number): void;
-// };
+type ContractFormProps = {
+  availableNetworks: Chain[];
+  onSubmit(contractAddress: string, networkId: number): void;
+};
 
-// const ContractForm = ({ onSubmit }: ContractFormProps) => {
-//   const [contractAddress, setContractAddress] = useState("");
-//   const [networkIdIndex, setNetworkIdIndex] = useState(
-//     DEFAULT_NETWORK_ID_INDEX
-//   );
-//   const [errorMsg, setErrorMsg] = useState("");
-//   const networkItems = useMemo(
-//     () =>
-//       NETWORK_IDS.map((id) => (
-//         <NetworkItem
-//           key={id}
-//           label={NETWORKS[id].name}
-//           icon={NETWORKS[id].logo}
-//           isTestnet={isTestnet(id)}
-//         />
-//       )),
-//     []
-//   );
-//   const disableSubmit =
-//     networkIdIndex === DEFAULT_NETWORK_ID_INDEX || !contractAddress.length;
+export const ContractForm = ({
+  availableNetworks = [],
+  onSubmit,
+}: ContractFormProps) => {
+  const [contractAddress, setContractAddress] = useState("");
+  const [networkIdIndex, setNetworkIdIndex] = useState(
+    DEFAULT_NETWORK_ID_INDEX
+  );
+  const [errorMsg, setErrorMsg] = useState("");
+  const disableSubmit =
+    networkIdIndex === DEFAULT_NETWORK_ID_INDEX || !contractAddress.length;
 
-//   useEffect(() => {
-//     setErrorMsg("");
-//   }, [contractAddress, networkIdIndex]);
+  console.log(availableNetworks)
+  useEffect(() => {
+    setErrorMsg("");
+  }, [contractAddress, networkIdIndex]);
 
-//   const handleSubmit: FormEventHandler = (e) => {
-//     e.preventDefault();
+  const handleSubmit: FormEventHandler = (e) => {
+    e.preventDefault();
 
-//     if (
-//       !contractAddress ||
-//       !utils.isAddress(contractAddress) ||
-//       networkIdIndex === DEFAULT_NETWORK_ID_INDEX
-//     ) {
-//       setErrorMsg("Invalid contract address.");
-//       return;
-//     }
+    if (
+      !contractAddress ||
+      !utils.isAddress(contractAddress) ||
+      networkIdIndex === DEFAULT_NETWORK_ID_INDEX
+    ) {
+      setErrorMsg("Invalid contract address.");
+      return;
+    }
 
-//     onSubmit(contractAddress, NETWORK_IDS[networkIdIndex]);
-//   };
+    onSubmit(contractAddress, networkIdIndex);
+  };
 
-//   return (
-//     <Box
-//       css={css`
-//         width: 25%;
-//       `}
-//     >
-//       <div
-//         css={css`
-//           display: flex;
-//           flex-direction: column;
-//         `}
-//       >
-//         <form onSubmit={handleSubmit}>
-//           <div
-//             css={css`
-//               display: flex;
-//               flex-direction: column;
-//               margin-bottom: 3gu;
-//             `}
-//           >
-//             <Field label="Contract to describe" required>
-//               <TextInput
-//                 value={contractAddress}
-//                 placeholder="Type in contract address…"
-//                 onChange={(e: ChangeEvent<HTMLInputElement>) => {
-//                   setContractAddress(e.target.value);
-//                 }}
-//                 wide
-//               />
-//             </Field>
-//             <Field label="Network" required>
-//               <DropDown
-//                 placeholder="Select a network"
-//                 header="Network"
-//                 items={networkItems}
-//                 selected={networkIdIndex}
-//                 onChange={setNetworkIdIndex}
-//                 wide
-//               />
-//             </Field>
-//             {errorMsg && <Info mode="error">{errorMsg}</Info>}
-//           </div>
-//           <Button
-//             type="submit"
-//             label="Describe"
-//             mode="strong"
-//             disabled={disableSubmit}
-//             wide
-//           />
-//         </form>
-//       </div>
-//     </Box>
-//   );
-// };
+  return (
+    <StyledBox>
+      <div style={{ display: "flex", flexDirection: "column" }}>
+        <form onSubmit={handleSubmit}>
+          <div
+            style={{
+              display: "flex",
+              flexDirection: "column",
+              marginBottom: 3 * GU,
+            }}
+          >
+            <Field label="Contract to describe" required>
+              <TextInput
+                value={contractAddress}
+                placeholder="Type in contract address…"
+                onChange={(e: ChangeEvent<HTMLInputElement>) => {
+                  setContractAddress(e.target.value);
+                }}
+                wide
+              />
+            </Field>
+            <Field label="Network" required>
+              <DropDown
+                placeholder="Select a network"
+                header="Network"
+                items={availableNetworks.map(({ id, name, testnet }) => (
+                  <NetworkItem
+                    key={id}
+                    label={name}
+                    icon={getNetworkLogo(id)}
+                    isTestnet={testnet}
+                  />
+                ))}
+                selected={networkIdIndex}
+                onChange={setNetworkIdIndex}
+                wide
+              />
+            </Field>
+            {errorMsg && <Info mode="error">{errorMsg}</Info>}
+          </div>
+          <Button
+            type="submit"
+            label="Describe"
+            mode="strong"
+            disabled={disableSubmit}
+            wide
+          />
+        </form>
+      </div>
+    </StyledBox>
+  );
+};
 
-// export default ContractForm;
+const StyledBox = styled(Box)`
+  width: 25%;
+`;
--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,10 +1,33 @@
+import { json, useLoaderData } from "remix";
 import styled from "styled-components";
+import { allChains } from "wagmi";
 import { AppScreen } from "~/components/AppLayout/AppScreen";
+import { ContractForm } from "~/components/ContractForm";
+
+const networkExplorerRegex = /([A-Z]+)_EXPLORER_API_KEY/;
+
+export async function loader() {
+  const availableExplorers = Object.keys(process.env)
+    .filter((key) => networkExplorerRegex.test(key))
+    .map((key) => key.split("_")[0].toLowerCase());
+
+  return json(
+    allChains.filter(
+      (chain) =>
+        // Check if chain has the same explorer
+        !!chain.blockExplorers?.find(({ name }) =>
+          availableExplorers.includes(name.toLowerCase())
+        )
+    )
+  );
+}
 
 export default function Index() {
   return (
     <AppScreen>
-      <MainContainer></MainContainer>
+      <MainContainer>
+        <ContractForm availableNetworks={useLoaderData()} onSubmit={() => {}} />
+      </MainContainer>
     </AppScreen>
   );
 }
