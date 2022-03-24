763db4fed6dcd6fdd32acf87264d4ff6a32e6e7b
PJColombo <paulo.colombo@pm.me>
2022-03-24 15:38:45 +0100

    fix(Home): display home screen on `/home` route

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,40 +1,10 @@
-import { json, useLoaderData } from "remix";
-import styled from "styled-components";
-import { allChains } from "wagmi";
-import { AppScreen } from "~/components/AppLayout/AppScreen";
-import { ContractForm } from "~/components/ContractForm";
-
-const networkExplorerRegex = /([A-Z]+)_EXPLORER_API_KEY/;
-
-export async function loader() {
-  const availableExplorers = Object.keys(process.env)
-    .filter((key) => networkExplorerRegex.test(key))
-    .map((key) => key.split("_")[0].toLowerCase());
-
-  return json(
-    allChains.filter(
-      (chain) =>
-        // Check if chain has the same explorer
-        !!chain.blockExplorers?.find(({ name }) =>
-          availableExplorers.includes(name.toLowerCase())
-        )
-    )
-  );
-}
+import { useEffect } from "react";
+import { useNavigate } from "remix";
 
 export default function Index() {
-  return (
-    <AppScreen>
-      <MainContainer>
-        <ContractForm availableNetworks={useLoaderData()} onSubmit={() => {}} />
-      </MainContainer>
-    </AppScreen>
-  );
-}
+  const navigate = useNavigate();
 
-const MainContainer = styled.div`
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  height: 100%;
-`;
+  useEffect(() => navigate("/home"), [navigate]);
+
+  return null;
+}
