86e9fd0d366f240ce10021980360074fa18da243
Gabi <gabrielpk.18@gmail.com>
2022-04-08 22:31:24 -0300

    fix: handle subgraph fetches & cache providers

--- app/providers/Wagmi.tsx
+++ app/providers/Wagmi.tsx
@@ -2,8 +2,32 @@ import { providers } from "ethers";
 import { useMemo } from "react";
 import type { ReactNode } from "react";
 import { WagmiProvider } from "wagmi";
+import { allChains } from "wagmi";
+import type { Connector } from "wagmi";
+import { InjectedConnector } from "wagmi/connectors/injected";
+import { WalletConnectConnector } from "wagmi/connectors/walletConnect";
 
-import { getConnectors } from "~/utils/client/connectors.client";
+const getConnectors = (chainId: string): Connector[] => {
+  const chain = allChains.find((chain) => chain.id === Number(chainId));
+
+  if (!chain) {
+    throw new Error("CHAIN_ID not found: " + chainId);
+  }
+
+  return [
+    new InjectedConnector({
+      chains: [chain],
+      options: { shimDisconnect: true },
+    }),
+    new WalletConnectConnector({
+      chains: [chain],
+      options: {
+        infuraId: window.ENV.INFURA_ID,
+        qrcode: true,
+      },
+    }),
+  ];
+};
 
 const Wagmi = ({ children }: { children: ReactNode }) => {
   const connectors = useMemo(() => getConnectors(window.ENV.CHAIN_ID), []);
