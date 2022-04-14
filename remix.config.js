ee588f370dc68bd560ce18bc0694eab8f4a6063e
0xGabi <gabrielpk.18@gmail.com>
2022-04-14 20:00:40 -0300

    chore: add @remix-run/node

--- remix.config.js
+++ remix.config.js
@@ -3,14 +3,10 @@
  */
 module.exports = {
   serverBuildTarget: "netlify",
-  // When running locally in development mode, we use the built in remix
-  // server. This does not understand the vercel lambda module format,
-  // so we default back to the standard build output.
-  server: process.env.NODE_ENV === "development" ? undefined : "./server.js",
+  server: "./server.js",
   ignoredRouteFiles: [".*"],
   // appDirectory: "app",
   // assetsBuildDirectory: "public/build",
-  // serverBuildPath: "api/index.js",
+  // serverBuildPath: ".netlify/functions-internal/server.js",
   // publicPath: "/build/",
-  // devServerPort: 8002,
 };
