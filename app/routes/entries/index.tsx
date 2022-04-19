511e1768a44d21158379964885da9a68cdecf883
0xGabi <gabrielpk.18@gmail.com>
2022-04-19 16:03:19 -0300

    feat: entries styles

--- app/routes/entries/index.tsx
+++ app/routes/entries/index.tsx
@@ -4,13 +4,12 @@ import { json } from "@remix-run/node";
 import { useLoaderData, useNavigate } from "@remix-run/react";
 import styled from "styled-components";
 import { AppScreen } from "~/components/AppLayout/AppScreen";
+import { StatusLabel } from "~/components/StatusLabel";
 import type { FnEntry } from "~/types";
 import { fetchFnEntries } from "~/utils/server/subgraph.server";
 
 export const loader: LoaderFunction = async () => {
   const fns = await fetchFnEntries();
-
-  // TODO: group by contract
 
   return json({ fns });
 };
@@ -33,12 +32,16 @@ type LoaderData = {
 
 export function EntriesList() {
   const { fns } = useLoaderData<LoaderData>();
+  const { below, within } = useViewport();
 
   return (
     <AppScreen hideBottomBar>
       <Container>
         {fns.length ? (
-          <ListContainer>
+          <ListContainer
+            compactMode={below("medium")}
+            tabletMode={within("medium", "large")}
+          >
             {fns.map((f) => (
               <EntryCard key={f.id} fn={f} />
             ))}
@@ -56,18 +59,16 @@ function EntryCard({ fn }: { fn: FnEntry }) {
 
   return (
     <EntryContainer onClick={() => navigate(`/entries/${fn.id}`)}>
-      {fn.sigHash}
+      <NoticeContainer>
+        swapExactETHForTokens(uint256,address[],address,uint256)
+      </NoticeContainer>
+      <InfoContainer>
+        <div>{fn.sigHash}</div>
+        <StatusLabel status={fn.status} />
+      </InfoContainer>
     </EntryContainer>
   );
 }
-
-const Container = styled.div`
-  display: flex;
-  justify-content: center;
-  align-items: start;
-  height: 100%;
-  width: 100%;
-`;
 
 const MainContainer = styled.div<{ compactMode: boolean }>`
   display: flex;
@@ -78,21 +79,46 @@ const MainContainer = styled.div<{ compactMode: boolean }>`
   height: 100%;
 `;
 
-const ListContainer = styled.div`
+const Container = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: start;
+  height: 100%;
+  width: 100%;
+`;
+
+const ListContainer = styled.div<{ compactMode: boolean; tabletMode: boolean }>`
   display: grid;
-  grid-gap: ${2 * GU}px;
-  grid-template-columns: repeat(auto-fill, minmax(280px, 1fr));
-  margin-bottom: ${2 * GU}px;
+  grid-gap: ${5 * GU}px;
+  grid-template-columns: ${({ compactMode, tabletMode }) =>
+    `repeat(${compactMode ? "1" : tabletMode ? "2" : "3"}, ${
+      compactMode ? "327" : tabletMode ? "336" : "350"
+    }px)`};
+  margin-bottom: ${5 * GU}px;
 `;
 
 const EntryContainer = styled.div`
-  padding: ${5 * GU}px ${4 * GU}px ${3 * GU}px ${4 * GU}px;
+  padding: ${3 * GU}px;
   background: ${(props) => props.theme.surface};
   border: 1px solid ${(props) => props.theme.border};
   border-radius: ${2.5 * GU}px;
   box-shadow: 0px 1px 2px rgba(0, 0, 0, 0.15);
   cursor: pointer;
   display: grid;
-  grid-gap: ${2 * GU}px;
-  text-align: center;
+  grid-gap: ${3 * GU}px;
+  height: 223px;
+`;
+
+const NoticeContainer = styled.div`
+  display: -webkit-box;
+  -webkit-line-clamp: 3;
+  -webkit-box-orient: vertical;
+  overflow: hidden;
+`;
+
+const InfoContainer = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  border-top: 1px solid ${(props) => props.theme.border};
 `;
